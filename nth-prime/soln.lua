c6ce3078d26e4df9691c89ae2704e0f5b7c8424a
Somesh Kar <gh@somesh.co>
2021-08-02 16:03:17 +0530

    Add comments

--- nth-prime/soln.lua
+++ nth-prime/soln.lua
@@ -5,30 +5,39 @@ function isPrime(n)
     return true
   end
   
+  -- check if divisible by numbers till square root of n
   for i = 2, n^(1/2), 1
   do
+    -- if divisible, it's not prime
     if (n % i) == 0 then
       return false
     end
   end
+  -- not divisible by any number except 1 and itself, so it's prime
   return true
 end
 
+-- take input from user
 io.write("Enter N: ")
 n = io.read("*number")
 
+-- count how many primes you have
 c = 0
 i = 1
+-- keep trying until you get the nth prime
 while (true)
 do
+  -- if found, exit loop and print value
   if c == n then
     print(i-1)
     break
   end
-  
+ 
+  -- incrment count
   if isPrime(i) then
     c = c + 1
   end
 
+  -- increment the iterator
   i = i + 1
 end
